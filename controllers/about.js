a8859eecc998f2cb30042191cedd0ffc73f22df2
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-12-30 13:37:55 -0500

    🏗 added three new routes to navbar

---
+++ controllers/about.js
@@ -0,0 +1,20 @@
+/*
+* About
+*/
+
+module.exports = (app) => {
+    // Why use FoodPrint?
+    app.get('/whyfoodprint', (req, res) => {
+        /** route here :D */
+    })
+
+    // What is CO2e?
+    app.get('/whatCO2e', (req, res) => {
+        /** route here :D */
+    })
+
+    // Resources
+    app.get('/resources', (req, res) => {
+        /** route here :D */
+    })
+};
